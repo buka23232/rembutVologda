1bfe685cb79e9b6ca29d8ef3f963987433979417
buka23232 <118247935+buka23232@users.noreply.github.com>
2022-11-15 09:44:52 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,85 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    this.el.classList.remove('btn-up_hide');
+  },
+  hide() {
+    this.el.classList.add('btn-up_hide');
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    document.querySelector('.btn-up').onclick = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }
+}
+btnUp.addEventListener();
+window.onscroll = function(ev) {
+    if (window.scrollY == 0) {
+         document.getElementById("navbar").style.background = "white"; 
+    }
+    else    
+    {
+        document.getElementById("navbar").style.background = "dodgerblue"; 
+    }
+}
+$("body").on('click', '[href*="#anchor"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
+$("body").on('click', '[href*="#anchorq"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
+$("body").on('click', '[href*="#onas"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
+$("body").on('click', '[href*="#contats"]', function(e){
+    var fixed_offset = 100;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
+function ShowHide(id){
+	var nameBlock=document.getElementById(id);
+	var styleBlock=getComputedStyle(nameBlock).display;
+	nameBlock.style.display=styleBlock==='none'?'block':'none';
+}
+function ShowHides(id){
+	var nameBlock=document.getElementById(id);
+	var styleBlock=getComputedStyle(nameBlock).display;
+	nameBlock.style.display=styleBlock==='none'?'block':'none';
+}
